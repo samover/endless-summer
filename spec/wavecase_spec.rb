71a6790b85e7206380ece88ba038ef14996ad041
Ptolemy <ptolemybarnes@gmail.com>
2015-09-17 21:39:27 +0100

    Updates spec

--- spec/wavecase_spec.rb
+++ spec/wavecase_spec.rb
@@ -3,14 +3,22 @@ require './lib/to_wavecase'
 describe String do
 
   it "converts a single word to wavecase" do
-    expect("surfspot".to_wavecase).to eq "SuRfSpOt"
+    expect("surfspot".to_wavecase).to eq "sUrFsPoT"
   end
 
   it 'converts a sentence to wavecase' do
-    expect("Colourless green ideas sleep furiously".to_wavecase).to eq "CoLoUrLeSs GrEeN iDeAs SlEeP fUrIoUsLy."
+    expect("colourless green ideas sleep furiously".to_wavecase).to eq(
+      "cOlOuRlEsS gReEn IdEaS sLeEp FuRiOuSlY"
+    )
+  end
+  
+  it 'ignores existing casing' do
+    expect("Colourless GreeN ideas sleEp furiOusly".to_wavecase).to eq(
+      "cOlOuRlEsS gReEn IdEaS sLeEp FuRiOuSlY"
+    )
   end
 
-  it 'ignores over numbers' do
+  it 'ignores numbers' do
     expect("endle5s 5ummer".to_wavecase).to eq "eNdLe5s 5uMmEr"
   end
 
